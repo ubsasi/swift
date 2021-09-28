d3e28c1ac1fa0af08e00b6695e0e7db2d683b225
Max Desiatov <max@desiatov.com>
2021-09-28 15:04:16 +0100

    Bring back weakly-linked `fatalError` to Mutex.cpp

--- stdlib/public/Concurrency/Mutex.cpp
+++ stdlib/public/Concurrency/Mutex.cpp
@@ -18,3 +18,8 @@
 #ifdef SWIFT_STDLIB_SINGLE_THREADED_RUNTIME
   #include "swift/Runtime/MutexSingleThreaded.h"
 #endif
+
+__attribute__ ((weak))
+SWIFT_NORETURN void swift::fatalError(uint32_t flags, const char *format, ...) {
+  abort();
+}
